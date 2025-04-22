565ed6bb462ceede5163e75096e72e61d3d0dbf3
Amy Romanello <arom2013@gmail.com>
2025-04-22 21:46:55 +0200

    update dependencies

--- scripts/main_caller.py
+++ scripts/main_caller.py
@@ -1,5 +1,14 @@
+
+
+
+
+
+
 def main():
     print("Hello from light-minded!")
+
+    #TODO: online vs offline user modes
+    #TODO: analysis setup mode --> which parcellation, etc.
 
 
 if __name__ == "__main__":
